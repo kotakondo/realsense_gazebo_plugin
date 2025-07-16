8c357e69da2160f54328db70915bd9b28eb975bf
kotakondo <kkondo@mit.edu>
2025-07-16 17:53:36 -0400

    revert the qos change

--- src/gazebo_ros_realsense.cpp
+++ src/gazebo_ros_realsense.cpp
@@ -56,9 +56,6 @@ void GazeboRosRealsense::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
 
   this->itnode_.reset(new image_transport::ImageTransport(this->node_));
 
-  rclcpp::QoS sensor_qos(rclcpp::KeepLast(1));
-  sensor_qos.best_effort().durability_volatile();
-
   this->color_pub_ = this->itnode_->advertiseCamera(
     cameraParamsMap_[COLOR_CAMERA_NAME].topic_name, 2);
   this->ir1_pub_ = this->itnode_->advertiseCamera(
@@ -70,7 +67,7 @@ void GazeboRosRealsense::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
 
   if (pointCloud_) {
     this->pointcloud_pub_ = this->node_->create_publisher<sensor_msgs::msg::PointCloud2>(
-      pointCloudTopic_, sensor_qos);
+      pointCloudTopic_, rclcpp::SystemDefaultsQoS());
   }
 
   RCLCPP_INFO(node_->get_logger(), "Loaded Realsense Gazebo ROS plugin.");
